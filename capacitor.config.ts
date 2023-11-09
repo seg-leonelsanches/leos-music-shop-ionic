0ee7b76a4c4cf1485bf165c36f84829fb1a8e47f
Leonel Sanches <113376080+seg-leonelsanches@users.noreply.github.com>
2023-11-09 11:10:45 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'leos-music-shop-ionic',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
